f99f88ce1c445c2cdfa3217e5baa5cec3735867a
IuriMarreiro <iurimarreironunes@gmail.com>
2025-03-18 04:20:27 +0000

    desafio nivel mestre xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,14 +1,72 @@
 #include <stdio.h>
 
+// Codifição para movimento das peças
+// função para movimentar torre
+
+void movimentoTorre (char *direcao[50], int torre) {
+
+    if (torre < 5)
+    {
+        printf("%s\n", direcao);
+        movimentoTorre (direcao, torre + 1);
+    }    
+}
+
+// função para movimentar o bispo
+
+void movimentoBispo (char *direcao[50], char *direcao2[50], int bispo) {
+
+    while (bispo < 5)
+    {
+        if (bispo < 5)
+        {
+            printf("%s ", direcao);
+        }
+        
+        printf("%s\n", direcao2);
+        bispo++;
+    }    
+}
+
+// função para movimentar a rainha
+
+void movimentoRainha (char *direcao[50], char *direcao2[50], int rainha) {
+
+    while (rainha < 8)
+    {
+        if (rainha < 8)
+        {
+            printf("%s ", direcao);
+        }
+        
+        printf("%s\n", direcao2);
+        rainha++;    
+    }
+}
+
+// função para movimentar o cavalo
+
+void movimentoCavalo (char *direcao[50], char *direcao2[50], int cavalo) {
+
+    for (cavalo = 0; cavalo <= 2; cavalo++)
+    {
+        if (cavalo == 2) {
+            
+            printf("%s\n", direcao2);
+            break;
+        } 
+        printf("%s\n", direcao);
+    }  
+}
+
 int main () {
 
-// Iniciando a codificação dos movimentos das peças de xadrez
 // Iniciei com um menu simples para o programa ficar mais robusto
 
     printf("*** GAME DE XADREZ ***\n\n");
 
-    int torre, bispo, rainha, cavalo, moverCavalo;
-    char direcao[50], direcao1[50], direcao2[50], escolha;
+    int torre, bispo, rainha, cavalo;
+    char direcao[50], direcao2[50], escolha;
 
 // Menu para o usuario escolher qual peça utilizar
 
@@ -29,98 +87,75 @@ int main () {
 // Usuario poderá escolher o caminho da peça
 
     printf("Escolha a direção da torre:\n");
-    scanf("%s", &direcao);
-    printf("\n");
+    scanf("%s", direcao);
+    printf("Torre movimenta 5 casas para:\n");
 
-// loop for para movimentar a torre
+    torre = 0;
+    movimentoTorre (&direcao, torre);
 
-            for (torre = 1; torre <= 5; torre++)
-    {
-                printf("%s\n", direcao);
-    }
-    
-// movimento da torre será de 5 casas
+// o movimento da torre será de 5 casas para direção que o usuario escolher
 
         break;
 
     case 'B':
     case 'b':
 
-// Escolher o movimento do bispo poderá ser uma frase
+// Escolher o movimento do bispo em duas etapas
 
-    bispo = 1;
     printf("Escolha a direção do bispo\n");
-    while(getchar() != '\n');
-    fgets(direcao, sizeof(direcao), stdin);
-    printf("\n");
+    printf("Caminho vertical será:\n");
+    scanf("%s", direcao);
+    printf("Caminho horizontal será:\n");
+    scanf("%s", direcao2);
+    printf("Bispo movimenta 5 casas para:\n");
 
-// loop while para movimentar o bispo
+    bispo = 0;
+    movimentoBispo (&direcao, &direcao2, bispo);
 
-        while (bispo <= 5)
-        {
-            printf("%s", direcao);
-                bispo++;
-        }
-
-// movimento do bispo será de 5 casas
+// movimento do bispo será de 5 casas para direção que o usuario escolher
 
         break;
          
     case 'R':
     case 'r':
 
-// o movimento da rainha tambem será uma frase
+// o movimento da rainha também será de duas etapas
 
-        rainha = 1;
-        printf("Escolha a direção da rainha \n");
-        while(getchar() != '\n');
-        fgets(direcao, sizeof(direcao), stdin);
-        printf("\n");
+    printf("Escolha a direção da rainha \n");
+    printf("Caminho vertical será:\n");
+    scanf("%s", direcao);
+    printf("Caminho horizontal será:\n");
+    scanf("%s", direcao2);
+    printf("Rainha movimenta 8 casas para:\n");
 
-// loop do-while para movimentar a rainha
+    rainha = 0;
+    movimentoRainha (&direcao, &direcao2, rainha);
 
-        do{
-            printf("%s", direcao);
-            rainha++;
-
-        } while (rainha <= 8);
-
-// movimento da rainha será de 8 casas
+// movimento da rainha será de 8 casas para a direção que o usuario escolher
 
         break;
 
     case 'C':
     case 'c':
 
-// o movimento do cavalo será de dois comandos
+// o movimento do cavalo será de duas etapas
 
-        moverCavalo = 1;
-        printf("Escolha a direção do cavalo, em dois comandos \n");
-        scanf("%s", &direcao1);
-        scanf("%s", &direcao2);
-        printf("\n");
-
-// loops aninhados para mover o cavalo
-
-        while (moverCavalo--)
-        {
-            for (cavalo = 0; cavalo < 2; cavalo++)
-                {
-                    printf("%s\n", direcao1);
-                }
-                
-            printf("%s\n", direcao2);
-            
-        }
+        printf("Escolha a direção do cavalo\n");
+        printf("Caminho vertical será:\n");
+        scanf("%s", direcao);
+        printf("Caminho horizontal será:\n");
+        scanf("%s", direcao2);
+        printf("Cavalo movimenta para:\n");
         
-// o cavalo move 2 casas para a vertical e 1 para horizontal
+        movimentoCavalo (&direcao, &direcao2, cavalo);
+        
+// o cavalo move 2 casas para a vertical e 1 para horizontal, para onde o usuario escolher
 
         break;
 
         default:
             printf("Escolha invalidade...");
-
     }
-   
+
     return 0;
 }
